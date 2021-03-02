9d468f960b7b2842f8196a3853daecbbc91fbf05
Wendryl <wendryl10000@gmail.com>
2021-03-02 20:39:39 -0300

    module and routing-module imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './Pages/home-page/home-page.component';
 import { LoginPageComponent } from './Pages/login-page/login-page.component';
 
 const routes: Routes = [
   {
     path: 'admin',
     component: LoginPageComponent
+  },
+  {
+    path: '',
+    component: HomePageComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './Pages/login-page/login-page.component';
 import { InputComponent } from './Components/input/input.component';
+import { HomePageComponent } from './Pages/home-page/home-page.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { SocialBarComponent } from './Components/social-bar/social-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
-    InputComponent
+    InputComponent,
+    HomePageComponent,
+    NavbarComponent,
+    FooterComponent,
+    SocialBarComponent,
   ],
   imports: [
     BrowserModule,
