7ac4bd847ec7371a37f4d5bc20b6d47784a9d833
Wendryl <wendryl10000@gmail.com>
2021-03-02 23:48:27 -0300

    add navbar mobile support

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  public displayMobileNavbar: boolean = false;
+  public navClass: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleNavbar(): void {
+    this.displayMobileNavbar == true ? this.displayMobileNavbar = false : this.displayMobileNavbar = true;
   }
 
 }
