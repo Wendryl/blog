5fbbaf442bb656e0ec121b6526cdf1755baf5c24
Wendryl <wendryl10000@gmail.com>
2021-03-04 20:11:40 -0300

    feat: implements custom input integration into login form

--- src/app/Components/input/input.component.ts
+++ src/app/Components/input/input.component.ts
@@ -1,25 +1,59 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, forwardRef, Input, OnInit } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+  styleUrls: ['./input.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: InputComponent,
+      multi: true
+    }
+  ]
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements ControlValueAccessor {
 
+  private _value: any = '';
+  onChange: any = (value: any) => { };
+  onTouched: any = () => { };
 
   @Input('label')
   inputLabel?: string;
 
+  @Input('id')
+  inputId?: string;
+
   @Input('type')
-  inputType: string = 'text';
+  inputType = 'text';
 
   @Input('placeholder')
   inputPlaceholder?: string;
 
-  constructor() { }
+  get value() {
+    return this._value;
+  }
 
-  ngOnInit(): void {
+  set value(value: any) {
+    this._value = value;
+    this.onChange(this._value);
+  }
+
+  doInput(event: any) {
+    this.value = event.value;
+  }
+
+  writeValue(value: any): void {
+    this._value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
   }
 
 }
