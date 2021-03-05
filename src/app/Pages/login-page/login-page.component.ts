bf030600c930d6d1b7e78dcae7157f16af9a0a66
Wendryl <wendryl10000@gmail.com>
2021-03-04 21:05:36 -0300

    Add login useCase signature

--- src/app/Pages/login-page/login-page.component.ts
+++ src/app/Pages/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import * as Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login-page',
@@ -9,19 +10,26 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class LoginPageComponent implements OnInit {
 
   loginForm!: FormGroup;
-
   constructor() { }
 
   ngOnInit(): void {
     this.loginForm = new FormGroup({
-      email: new FormControl(''),
-      password: new FormControl('')
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, Validators.required)
     })
   }
 
   submitForm(evt: Event) {
+
+    const email = this.loginForm.controls.email.value;
+    const password = this.loginForm.controls.password.value;
+
     evt.preventDefault();
-    console.log(this.loginForm);
+    if (this.loginForm.controls.email.value != 'wendryl10000@gmail.com' || this.loginForm.controls.password.value != '123123') {
+      setTimeout(() => {
+        return Swal.default.fire('Error!', 'Email or password invalid!', 'error');
+      }, 3000)
+    }
   }
 
 }
